bae2d8e7abc0ef1501182fa047746f241db7f48e
saiful10400 <saifulislam10400i5@gmail.com>
2025-04-08 23:08:12 +0600

    error edge case solved.

--- src/pages/Ai.tsx
+++ src/pages/Ai.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios  from "axios";
 import { Bot, Send, User } from "lucide-react";
 import { useEffect, useState } from "react";
 import robotGif from "../assets/robot.gif";
@@ -14,6 +14,7 @@ const Ai = () => {
   const [chat, setChat] = useState<tChat[]>([]);
   const [textAreaheight, setTextAreaHeight] = useState(48);
   const [inputText, setInputText] = useState<string | null>(null);
+  const[errorCode,setErrorcode]=useState<number|null>(null)
 
   const inputFieldHandle = (e: React.FormEvent<HTMLTextAreaElement>) => {
     const scrollHeight = e.currentTarget.scrollHeight;
@@ -26,22 +27,33 @@ const Ai = () => {
   };
 
   const promptSubmitHandle = async () => {
-    if (!inputText) return;
+    if (!inputText ||inputText.length===0) return;
 
     setChat((p) => {
       return [...p, { user: inputText }];
     });
     setInputText(null);
     setTextAreaHeight(48);
+    setErrorcode(null)
+
+    try{
+          
     const data = await axios.get(
       `https://mcq-test-server.vercel.app/api/result/ai?prompt=${inputText}`
     );
+
+ 
 
     setChat((p) => {
       p[p.length - 1].ai = data.data?.data.ai;
       p[p.length - 1].timeStamp = data.data?.data.timeStamp;
       return [...p];
     });
+    } catch(err){
+    if(!err) return
+      setErrorcode(504)
+    }
+
   };
 
   // chat ref handle.
@@ -53,10 +65,18 @@ const Ai = () => {
     });
   }, [chat]);
 
+  // enter button press handle.
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter") {
+      promptSubmitHandle();
+    }
+  };
+
   return (
     <div className="min-h-[70vh] pb-28">
       {chat.length === 0 ? (
-        <div className="flex justify-center items-center flex-col  mt-24">
+        <div className="flex justify-center items-center flex-col  mt-28">
           <img className="w-[250px] " src={robotGif} alt="" />
           <h1 className="w-[85vw] text-center mx-auto font-medium text-gray-500">
             আমি পিকু। যেকোনো জিজ্ঞাসায় আমি তোমার পাশে আছি!
@@ -66,7 +86,7 @@ const Ai = () => {
         <div>
           {chat?.map((item: tChat, idx: number) => {
             return (
-              <div key={idx}>
+              <div className="pt-5" key={idx}>
                 <div className="flex justify-end items-end">
                   <div className="chat chat-end w-full">
                     <div className="chat-bubble chat-bubble-warning bg-[#f14545] text-white min-w-[50vw]">
@@ -97,9 +117,11 @@ const Ai = () => {
           })}
         </div>
       )}
+      {errorCode&&<h1 className="text-red-600 font-medium mt-5 ml-4">একটি সমস্যা হয়েছে। আবার চেষ্টা করো। <br /> <span>error code:{errorCode}</span></h1>}
 
       <div className="fixed bottom-[68px] left-0 z-10 w-full flex items-center gap-2 bg-gray-200 pr-2 pl-1">
         <textarea
+          onKeyDown={handleKeyDown}
           value={inputText || ""}
           placeholder="তোমার প্রশ্নটা লিখো এখানে…"
           style={{ height: textAreaheight + "px" }}
